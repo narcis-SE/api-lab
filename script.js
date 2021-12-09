16024f5e2936287e1a3eaf68486cea7514198539
narcis <narcissprecic@gmail.com>
2021-12-09 15:50:04 -0500

    Cuties have been pushed

---
+++ script.js
@@ -0,0 +1,30 @@
+var displayContainer = document.getElementById("display");
+var anotherDisplay = document.getElementById("anotherDisplay");
+
+const promise = fetch("https://www.reddit.com/r/aww/.json")
+.then(response => response.json());
+
+promise.then(data=>{
+    for(i=0; i<data.data.children.length; i++){
+        const img = new Image();
+        img.classList.add("image");
+
+        const div = document.createElement("div");
+        div.classList.add("div");
+
+        const link = document.createElement("a");
+        link.classList.add("link");
+
+        console.log(data.data.children[i])
+
+        div.innerHTML = data.data.children[i].data.title;
+        img.src = data.data.children[i].data.thumbnail;
+        link.innerHTML = "A link to this cutie!"; 
+        link.href = "https://www.reddit.com" + data.data.children[i].data.permalink;
+
+        displayContainer.appendChild(div);
+        displayContainer.appendChild(img);
+        displayContainer.appendChild(link);
+    }
+})
+
