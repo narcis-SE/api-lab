1973a95ae2ccd759a9e90f17b6f45113dc29ca85
narcis <narcissprecic@gmail.com>
2021-12-09 16:46:37 -0500

    Added where you can put your own subreddit

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 var displayContainer = document.getElementById("display");
 var anotherDisplay = document.getElementById("anotherDisplay");
 
-const promise = fetch("https://www.reddit.com/r/aww/.json")
-.then(response => response.json());
 
-promise.then(data=>{
-    for(i=0; i<data.data.children.length; i++){
-        const img = new Image();
-        img.classList.add("image");
 
-        const div = document.createElement("div");
-        div.classList.add("div");
+
+function onClick(){
+    var subreddit = document.getElementById("subreddit");
+    let value = subreddit.value;
+    console.log(value);
+    const promise = fetch("https://www.reddit.com/r/"+ value + "/.json")
+    .then(response => response.json());
+
+    promise.then(data=>{
+        for(i=0; i<data.data.children.length; i++){
+         const img = new Image();
+         img.classList.add("image");
+
+         const div = document.createElement("div");
+         div.classList.add("div");
 
         const link = document.createElement("a");
         link.classList.add("link");
@@ -19,6 +26,7 @@ promise.then(data=>{
 
         div.innerHTML = data.data.children[i].data.title;
         img.src = data.data.children[i].data.thumbnail;
+
         link.innerHTML = "A link to this cutie!"; 
         link.href = "https://www.reddit.com" + data.data.children[i].data.permalink;
 
@@ -27,4 +35,4 @@ promise.then(data=>{
         displayContainer.appendChild(link);
     }
 })
-
+}
